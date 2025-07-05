377a4e8690d27b77dce020b7167aba4489954c6e
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2025-07-05 17:22:41 +0530

    Update contains duplicate.java

--- ArrayAndHashing/contains duplicate.java
+++ ArrayAndHashing/contains duplicate.java
@@ -1,1 +1,29 @@
+Brute force approach
 
+ public class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+Hashing set approach ( most optimal one)
+
+public class Solution {
+    public boolean hasDuplicate(int[] nums) {   // same as brute force
+        Set<Integer> seen = new HashSet<>();    // used to create new hash set
+        for (int num : nums) {      // enhanced for loop will cover all elements of nums in numm that is a hashset.
+            if (seen.contains(num)) {  //Checks if the current number `num` **already exists** in the `seen` set.
+                return true;
+            }
+            seen.add(num);       //If num was not already in seen, we add it to the set.
+        }
+        return false;
+    }
+}
