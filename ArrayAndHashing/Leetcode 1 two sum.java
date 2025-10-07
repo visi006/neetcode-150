e554159cf599fc30211a3f73b5333b8478f840f1
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2025-10-07 23:21:04 +0530

    Update Leetcode 1 two sum.java

--- ArrayAndHashing/Leetcode 1 two sum.java
+++ ArrayAndHashing/Leetcode 1 two sum.java
@@ -1,1 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // Create a HashMap to store numbers and their indices
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // Iterate through the array
+        for (int i = 0; i < nums.length; i++) {
+            // Calculate the complement of the current number
+            int complement = target - nums[i];
+
+            // Check if the complement is already in the map
+            if (map.containsKey(complement)) {
+                // If found, return the indices of the complement and the current number
+                return new int[] { map.get(complement), i };
+            }
+
+            // Otherwise, add the current number and its index to the map
+            map.put(nums[i], i);
+        }
+
+        // If no solution found, return an empty array
+        return new int[] {};
+    }
+}
 
