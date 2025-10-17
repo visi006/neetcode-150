be2e94209a0f316b25f38701bcd7e1dacdca54c1
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2025-10-17 22:09:48 +0530

    Add moveZeroes method to Solution class
    Implement moveZeroes method to shift zeros in array.

--- ArrayAndHashing/leetcode 283 Move zeros.java
+++ ArrayAndHashing/leetcode 283 Move zeros.java
@@ -1,1 +1,22 @@
-lee
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int size = nums.length;
+        if(size == 0 || size == 1) {
+            return;
+        }
+
+        int nz=0,z=0;
+        while(nz < size) {
+            if(nums[nz] != 0){
+                int temp = nums[nz];
+                nums[nz] = nums[z];
+                nums[z] = temp;
+                nz++;
+                z++;
+            }
+            else{
+                nz++;
+            }
+        }
+    }
+}
