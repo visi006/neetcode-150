efa79da2ff2da54a150bf095592f9506bf2cece1
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2025-10-17 22:13:02 +0530

    Update Leetcode 169 Majorityelement.java

--- ArrayAndHashing/Leetcode 169 Majorityelement.java
+++ ArrayAndHashing/Leetcode 169 Majorityelement.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int majorityElement(int[] nums) {
+        int counter = 1;
+        int candidate = nums[0];
+        for(int i=1;i<nums.length;i++) {
+            if(counter == 0) {
+                candidate = nums[i];
+            }
+             if(nums[i] == candidate){
+                counter++;
+             }
+             else {
+                counter--;
+             }
+        }
+             return candidate;
+            }
+    }
