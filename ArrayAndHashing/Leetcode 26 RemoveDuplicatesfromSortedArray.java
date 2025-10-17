34aa43242e39d722a8d383af286b014aab9b2d9b
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2025-10-17 22:47:24 +0530

    Update Leetcode 26 RemoveDuplicatesfromSortedArray.java

--- ArrayAndHashing/Leetcode 26 RemoveDuplicatesfromSortedArray.java
+++ ArrayAndHashing/Leetcode 26 RemoveDuplicatesfromSortedArray.java
@@ -1,1 +1,27 @@
 
+
+public class RemoveDuplicatesFromSorted {
+    static int removeDuplicate(int[] arr){
+        int rd = 0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[rd] != arr[i]) {
+                rd++;
+                arr[rd] = arr[i];
+            }
+        }
+        return rd+1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,2,3,3,4,6,6};
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + "");
+        }
+        int rd = removeDuplicate(arr);
+        System.out.println();
+        for(int i=0;i<rd;i++){
+             System.out.print(arr[i] + "");
+    }
+}
+}
+
