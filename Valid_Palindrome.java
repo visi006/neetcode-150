14de80938d32542093b8307e6c19dc84007188ce
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-01-12 22:01:41 +0530

    Implement palindrome check with alphanumeric validation
    Add methods to check alphanumeric characters and validate palindrome.

--- Valid_Palindrome.java
+++ Valid_Palindrome.java
@@ -21,3 +21,41 @@ Explanation: "raceacar" is not a palindrome.
 
   Java code :
 
+
+class Solution {
+    public boolean isAlphaNumericCharacter(char c){
+        return(c>='a' && c<='z')||
+        (c>='A' && c<='Z')|| (c>='0' && c<='9');
+    }
+    public boolean isPalindrome(String s) {
+
+     int i = 0;
+     int j = s.length() - 1;
+
+     while(i<j){
+        char c1 = s.charAt(i);
+        char c2 = s.charAt(j);
+        
+        if(!isAlphaNumericCharacter(c1)){
+            i= i+1;
+            continue;
+        }
+
+        if(!isAlphaNumericCharacter(c2)){
+            j=j-1;
+            continue;
+        }
+        if(Character.toLowerCase(c1) != Character.toLowerCase(c2) ){
+            return false;
+        }
+
+        i=i+1;
+        j=j-1;
+     }   
+
+     return true;
+    }
+}
+
+
+
