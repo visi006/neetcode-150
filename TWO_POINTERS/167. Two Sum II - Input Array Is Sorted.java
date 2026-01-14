25309a63ce186a67924fce82f1dfbe6bf4676ea4
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-01-14 21:18:06 +0530

    Update 167. Two Sum II - Input Array Is Sorted.java

--- TWO_POINTERS/167. Two Sum II - Input Array Is Sorted.java
+++ TWO_POINTERS/167. Two Sum II - Input Array Is Sorted.java
@@ -1,1 +1,62 @@
+USING TWO POINTER APPROACH :
+
+PROBLEM STATEMENT:
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+JAVA CODE :
+
+  class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+     int left = 0;
+     int right = numbers.length-1;
+
+     while(left<right) {
+        if(numbers[left]+numbers[right] > target){
+            right = right -1;
+        }
+        else if(numbers[left]+ numbers[right]< target){
+            left = left + 1;
+        }
+        else{
+            return new int[]{left+1,right+1};
+        }
+     }
+     return null;
+    }
+}
 
