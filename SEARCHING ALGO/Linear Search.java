8fb593d65d82d020366ed9b6002d5d559d6b6ef5
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-01-16 22:16:55 +0530

    Update Linear Search.java

--- SEARCHING ALGO/Linear Search.java
+++ SEARCHING ALGO/Linear Search.java
@@ -1,1 +1,22 @@
-
+import java.util.*;
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int key = sc.nextInt();
+        int found = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i]==key){
+                System.out.println("Value found at "+i+" index");
+                found = 1;
+            }
+        }
+        if(found==0){
+            System.out.println("Key is not found");
+        }
+    }
+}
