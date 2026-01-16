1ace7d521f12505693d69bf8c3015dad49f1ade0
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-01-16 22:17:35 +0530

    Implement binary search algorithm in Java

--- SEARCHING ALGO/Binary Search.java
+++ SEARCHING ALGO/Binary Search.java
@@ -1,1 +1,29 @@
-
+import java.util.*;
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int key = sc.nextInt();
+        int found = 0;
+        int low = 0, high = n-1;
+        while( low <= high){
+            int mid = (low+high)/2;
+            if(arr[mid]==key){
+                System.out.println("Key found at "+mid);
+                found = 1;
+                break;
+            } else if(arr[mid] > key){
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        if(found == 0){
+            System.out.println("Key isn't in the array");
+        }
+    }
+}
