013813e11f3cf80ac38b89de295552296c329201
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-02-05 23:21:14 +0530

    Update bruteforce.java

--- 3651-transformed-array/bruteforce.java
+++ 3651-transformed-array/bruteforce.java
@@ -1,1 +1,33 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
 
+      int n = nums.length;
+      int [] ans = new int[n];
+
+      for(int i=0;i<n;i++){
+        int pos = i;
+        int steps = nums[i];
+
+        if(steps>0){
+          for(int s=0;s<steps;s++){
+            pos++;
+          }
+          if(pos==n) 
+            pos=0;
+        }
+        else{
+          if(steps<0){
+          for(int s=0;s<-steps;s++){
+            pos--;
+          }
+              if (pos < 0) pos = n - 1;
+                }
+            }
+
+            ans[i] = nums[pos];
+        }
+
+        return ans;
+    }
+}
+          
