3366ae2e40aeeb70960de6934b525fb8987af331
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-02-05 23:11:20 +0530

    Time: 1 ms (98.72%) | Memory: 46.6 MB (71.79%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for(int i=0;i<nums.length;i++){
+            ans[i] = nums[((i+nums[i])% n + n) % n];        
+    }
+    return ans;
+   }
+}
