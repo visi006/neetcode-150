d8845b7a89a1bf7c69874a8a458841b57b45f1c8
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-02-10 14:14:10 +0530

    Refactor longestBalanced method for clarity

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
@@ -1,25 +1,25 @@
 class Solution {
     public int longestBalanced(int[] nums) {
         
-        int result = 0;
-        for(int i=0;i<nums.length;i++){
+        int result = 0; // for storing the final result used in returning the function
+        for(int i=0;i<nums.length;i++){  // just looping through the subarray accessing all the elements
 
-        HashSet<Integer> odd = new HashSet<>();
-        HashSet<Integer> even = new HashSet<>();
+        HashSet<Integer> odd = new HashSet<>();  // create a hashset for odd numbers 
+        HashSet<Integer> even = new HashSet<>(); // create a hashset for even numbers
 
-        for(int j=i;j<nums.length;j++){
-            if((nums[j]%2 == 0)){
-                even.add(nums[j]);
+        for(int j=i;j<nums.length;j++){ // consider j as initial position of subarray
+            if((nums[j]%2 == 0)){ // checking even number condition
+                even.add(nums[j]); // add to array if satisfied even
             }
             else {
-                odd.add(nums[j]);
+                odd.add(nums[j]);  // add to array if satisified odd
             }
-            if(odd.size() == even.size()){
-                result = Math.max(result,j-i+1);
+            if(odd.size() == even.size()){ // conditon to check if size of odd and even is same
+                result = Math.max(result,j-i+1);  // store the max value of the result in the result variable.
             }
         }
         }
-        return result;
+        return result; // return the result value which is stored from  the first.
         
     }
 }
